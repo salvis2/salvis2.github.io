bc105e2af7b4836c50a382ca297cb9eea191c319
Sebastian Alvis <sebialvis@gmail.com>
2018-10-09 21:36:38 +0000

    Changed File Structure to MVC

---
+++ public/javascripts/DnDRaces.js
@@ -0,0 +1,110 @@
+// Dungeons and Dragons Races
+// add bonus languages
+// add save bonuses, conditional
+// add visions?
+// How much do I just put under Racial Features?
+
+// Have an entry for other
+// just print that they should know it exists, but it won't go on the sheet?
+
+export class BaseRace {
+	constructor(raceName, size, speed,
+		abilityScoreAdjustments,
+		skillModifiers,
+		languages) {
+		this._raceName = raceName;
+		this._size = size;
+		this._speed = speed;
+		this._abilityScoreAdjustments = abilityScoreAdjustments;
+		this._skillModifiers = skillModifiers;
+		this._languages = languages;
+	}
+	get raceName() {
+		return this._raceName;
+	}
+	get size() {
+		return this._size;
+	}
+	get speed() {
+		return this._speed;
+	}
+	get abilityScoreAdjustments() {
+		return this._abilityScoreAdjustments;
+	}
+	get skillModifiers() {
+		return this._skillModifiers;
+	}
+	get languages() {
+		return this._languages;
+	}
+}
+
+export class Humon extends BaseRace {
+	constructor() {
+		super("Humon", "M", 30,
+			[],
+			[],
+			["Common"]
+		);
+	}
+}
+
+export class Dwarf extends BaseRace {
+	constructor() {
+		super("Dwarf", "M", 20,
+			[["Constitution", 2], ["Charisma", -2]],
+			[["Appraise", 2], ["Craft", 2]],
+			["Common", "Dwarven"]
+		);
+	}
+}
+
+export class Elf extends BaseRace {
+	constructor() {
+		super("Elf", "M", 30,
+			[["Dexterity", 2], ["Constitution", -2]],
+			[["Listen", 2], ["Search", 2], ["Spot", 2]],
+			["Common", "Elven"]
+		);
+	}
+}
+
+export class Gnome extends BaseRace {
+	constructor() {
+		super("Gnome", "S", 20,
+			[["Constitution", 2], ["Strength", -2]],
+			[["Listen", 2], ["Craft (alchemy)", 2]],
+			["Common", "Gnome"]
+		);
+	}
+}
+
+export class Half_Elf extends BaseRace {
+	constructor() {
+		super("Half-Elf", "M", 30,
+			[],
+			[["Listen", 1], ["Search", 1], ["Spot", 1], ["Diplomacy", 2], ["Gather Information", 2]],
+			["Common", "Elven"]
+		);
+	}
+}
+
+export class Half_Orc extends BaseRace {
+	constructor() {
+		super("Half-Orc", "M", 30,
+			[["Strength", 2], ["Intelligence", -2], ["Charisma", -2]],
+			[],
+			["Common", "Orc"]
+		);
+	}
+}
+
+export class Halfing extends BaseRace {
+	constructor() {
+		super("Halfling", "S", 20,
+			[["Dexterity", -2], ["Strength", -2]],
+			[["Climb", 2], ["Jump", 2], ["Move Silently", 2], ["Listen", 2]],
+			["Common", "Halfling"]
+		);
+	}
+}
---
+++ public/javascripts/dndTools.js
@@ -0,0 +1,65 @@
+// dndTools.js
+
+// Roll n d6s, primarily for ability score generation
+function rollDice(dieSize, numDice) {
+	let total = 0;
+
+	for (let i = 0; i < numDice; i += 1) {
+		total += Math.ceil(Math.random() * dieSize);
+	}
+
+	return total;
+}
+
+function rollAbScore(num) {
+	let abScore = 0;
+	let rolls = [];
+
+	for (let i = 0; i < num; i += 1) {
+		rolls.push(rollDice(6, 1));
+	}
+
+	rolls.sort(function (a, b) {
+  	return b - a;
+	});
+
+	for (let i = 0; i < 3; i += 1) {
+		abScore += rolls[i];
+	}
+	return abScore;
+}
+
+// Page Selecting Elements
+const dieNum = document.querySelector("#numDice");
+const dieSize = document.querySelector("#dieSize");
+const submit = document.querySelector("#submit");
+const statButton = document.querySelector("#rollStats");
+
+function displayDiceTotal(count) {
+	document.getElementById("diceOutput").innerHTML = ``;
+	for (let i = 0; i < count - 1; i += 1) {
+		document.getElementById("diceOutput").innerHTML += `${rollDice(dieSize.value, dieNum.value)}, `;
+	}
+	document.getElementById("diceOutput").innerHTML += `${rollDice(dieSize.value, dieNum.value)}`;
+}
+
+submit.addEventListener("click", () => {
+	const count = document.getElementById("numIt").value;
+	displayDiceTotal(count);
+});
+
+// Ability Score Roller
+function displayStats(num, count) {
+	document.getElementById("statOutput").innerHTML = ``;
+	for (let i = 0; i < count - 1; i += 1) {
+	  document.getElementById("statOutput").innerHTML += `${rollAbScore(num)}, `;
+	}
+	document.getElementById("statOutput").innerHTML += `${rollAbScore(num)}`;
+}
+
+statButton.addEventListener("click", () => {
+	const num = document.getElementById("charType").value;
+	const numStats = document.getElementById("numStats").value;
+	displayStats(num, numStats);
+});
+
