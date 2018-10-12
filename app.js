49d558b5a0f5cc6ee0d8f54c459285c6f87115b5
Sebastian Alvis <sebialvis@gmail.com>
2018-10-12 00:31:10 +0000

    Enabled a Nginx Proxy Server, Serving all Existing Content as Static Files

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 
+var serveStatic = require('serve-static');
 var bodyParser = require('body-parser');
 var path = require('path');
-
-// Routers
-var homeRouter = require('./routes/homeRouter');
-//var dndToolsRouter = require('./routes/dndToolsRouter');
-//var characterCreatorRouter = require('./routes/characterCreatorRouter');
-
-// Specify paths
-app.use('/', homeRouter);
-//app.use('/dndTools', dndToolsRouter);
-//app.use('/dndTools/characterCreator', characterCreatorRouter);
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +12,15 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(serveStatic('public/html'));
+app.use(serveStatic('public/stylesheets'));
+app.use(serveStatic('public/javascripts'));
+app.use(serveStatic('public/images'));
 
-// Test
-/*
-app.get('/', (req, res) => {
-	res.send('Server Upgrades in Progress');
+app.get('/', (req, res, next) => {
+	res.sendFile(__dirname + '/public/html/homepage.html');
 });
-*/
+
 
 // Use Routers
 //app.use('/', homeRouter);
@@ -52,4 +44,58 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 */
-module.exports = app;
+function normalizePort(val) {
+        var port = parseInt(val, 10);
+
+        if (isNaN(port)) {
+                return val;
+        }
+
+        if (port >= 0) {
+                return port;
+        }
+
+        return false;
+}
+
+function onError(error) {
+        if (error.syscall !== 'listen') {
+                throw error;
+        }
+
+        var bind = typeof port === 'string'
+                ? 'Pipe ' + port
+                : 'Port ' + port;
+
+        switch (error.code) {
+                case 'EACCES':
+                        console.error(bind + ' requires elevated privileges');
+                        process.exit(1);
+                        break;
+                case 'EADDRINUSE':
+                        console.error(bind + ' is already in use');
+                        process.exit(1);
+                        break;
+                default:
+                        throw error;
+        }
+}
+
+function onListening() {
+        var addr = server.address();
+        var bind = typeof addr === 'string'
+                ? 'pipe ' + addr
+                : 'port ' + addr.port;
+        console.log('Listening on ' + bind);
+}
+
+
+var port = normalizePort(process.env.PORT || '8080');
+app.set('port', port);
+
+var server = http.createServer(app);
+server.on('error', onError);
+server.on('listening', onListening);
+
+server.listen(port, '208.113.166.64');
+
