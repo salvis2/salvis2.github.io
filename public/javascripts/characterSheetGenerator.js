c8be4c6f1e511465d5b2976375979f4551c81761
Sebastian Alvis <sebialvis@gmail.com>
2018-10-15 20:28:50 +0000

    Test of PDF Toolkit via javascript and node

--- public/javascripts/characterSheetGenerator.js
+++ public/javascripts/characterSheetGenerator.js
@@ -1,29 +1,20 @@
 // Character Sheet Generator
-import pdftk from "node-pdftk";
 
-export function fillDndCharacterSheet(data) {
-	pdftk
-		.input("../etc/dndCharacterSheet.pdf")
-		.fillForm(data)
-		.output()
-		.then(buffer => {
-			console.log("Character Sheet Created");
-			res.type("application.pdf");
-			res.send(buffer);
-		})
-		.catch(next);
-}
-/*
 const pdfFiller = require("pdffiller");
 
-const dndCharacterSheetSource = "../etc/dndCharacterSheet.pdf";
-const dndCharacterSheetDestination = "../etc/createdDndCharacterSheet.pdf";
+const dndCharacterSheetSource = "public/other/dndCharacterSheet.pdf";
+const dndCharacterSheetDestination = "public/other/createdDndCharacterSheet.pdf";
 const shouldFlatten = false;
 
-	pdfFiller.fillFormWithFlatten(dndCharacterSheetSource, dndCharacterSheetDestination,
-		data, shouldFlatten, function (err) {
-			if (err) {
-				throw err;
-			}
-		});
-*/
+const data = {
+	"strScore": "16",
+	"dexScore": "13"
+};
+
+pdfFiller.fillFormWithFlatten(dndCharacterSheetSource, dndCharacterSheetDestination,
+	data, shouldFlatten, function (err) {
+	if (err) {
+		throw err;
+	}
+});
+
