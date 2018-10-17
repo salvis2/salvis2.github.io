389de4df8d6bc6e531174569bee779737dd3eb74
salvis2 <sebialvis@gmail.com>
2018-10-17 13:17:10 -0700

    Made function to create body data for POST
    Includes character class, level, name, ability scores, base attack bonus, base save bonuses, class skills, and spells per day / spells known where applicable.

--- public/javascripts/DnDCharacter.js
+++ public/javascripts/DnDCharacter.js
@@ -16,6 +16,8 @@ export class Character {
 		this._wis = wis;
 		this._cha = cha;
 
+		this.createBodyData = this.createBodyData.bind(this);
+
 		// Set Character Class Object
 		switch (characterClass) {
 		case "Barbarian":
@@ -202,7 +204,219 @@ export class Character {
 	}
 
 	createBodyData() {
-		let bodyData = {};
+		const classSkills = this.classSkillList;
+		const level = this.level;
+
+		console.log(classSkills.length);
+
+		let bodyData = [
+			// Basic Info
+			{ characterName: this.characterName },
+			{ classesLevels: `${this.characterClass} ${level}` },
+			// raceOrTemplate: this.characterRace,
+
+			// Ability Scores
+			// Add Race Adjustments after the Race Selection Component is Introduced
+			{ strBase: this.str },
+			{ dexBase: this.dex },
+			{ conBase: this.con },
+			{ intBase: this.inte },
+			{ wisBase: this.wis },
+			{ chaBase: this.cha },
+
+			// Race Basics
+
+			// Class Basics
+			{ bab: this.bab(level) },
+			{ fortBase: this.fortSave(level) },
+			{ refBase: this.refSave(level) },
+			{ willBase: this.willSave(level) },
+
+			// Class Skills
+			{ appraiseCS: classSkills[0] ? "On" : "Off" },
+			{ autohypnosisCS: classSkills[1] ? "On" : "Off" },
+			{ balanceCS: classSkills[2] ? "On" : "Off" },
+			{ bluffCS: classSkills[3] ? "On" : "Off" },
+			{ climbCS: classSkills[4] ? "On" : "Off" },
+			{ concentrationCS: classSkills[5] ? "On" : "Off" },
+			{ craft0CS: classSkills[6] ? "On" : "Off" },
+			{ craft1CS: classSkills[7] ? "On" : "Off" },
+			{ craft2CS: classSkills[8] ? "On" : "Off" },
+			{ decipherScriptCS: classSkills[9] ? "On" : "Off" },
+			{ diplomacyCS: classSkills[10] ? "On" : "Off" },
+			{ disableDeviceCS: classSkills[11] ? "On" : "Off" },
+			{ disguiseCS: classSkills[12] ? "On" : "Off" },
+			{ escapeArtistCS: classSkills[13] ? "On" : "Off" },
+			{ forgeryCS: classSkills[14] ? "On" : "Off" },
+			{ gatherInfoCS: classSkills[15] ? "On" : "Off" },
+			{ handleAnimalCS: classSkills[16] ? "On" : "Off" },
+			{ healCS: classSkills[17] ? "On" : "Off" },
+			{ hideCS: classSkills[18] ? "On" : "Off" },
+			{ intimidateCS: classSkills[19] ? "On" : "Off" },
+			{ jumpCS: classSkills[20] ? "On" : "Off" },
+			{ knowArcanaCS: classSkills[21] ? "On" : "Off" },
+			{ knowArchEngCS: classSkills[22] ? "On" : "Off" },
+			{ knowDungeonCS: classSkills[23] ? "On" : "Off" },
+			{ knowGeographyCS: classSkills[24] ? "On" : "Off" },
+			{ knowHistoryCS: classSkills[25] ? "On" : "Off" },
+			{ knowLocalCS: classSkills[26] ? "On" : "Off" },
+			{ knowNatureCS: classSkills[27] ? "On" : "Off" },
+			{ knowNobilityCS: classSkills[28] ? "On" : "Off" },
+			{ knowPlanesCS: classSkills[29] ? "On" : "Off" },
+			{ knowPsionicsCS: classSkills[30] ? "On" : "Off" },
+			{ knowReligionCS: classSkills[31] ? "On" : "Off" },
+			{ listenCS: classSkills[32] ? "On" : "Off" },
+			{ moveSilentlyCS: classSkills[33] ? "On" : "Off" },
+			{ openLockCS: classSkills[34] ? "On" : "Off" },
+			{ performActCS: classSkills[35] ? "On" : "Off" },
+			{ performComedyCS: classSkills[36] ? "On" : "Off" },
+			{ performDanceCS: classSkills[37] ? "On" : "Off" },
+			{ performKeyboardCS: classSkills[38] ? "On" : "Off" },
+			{ performOratoryCS: classSkills[39] ? "On" : "Off" },
+			{ performPercussionCS: classSkills[40] ? "On" : "Off" },
+			{ performStringCS: classSkills[41] ? "On" : "Off" },
+			{ performWindCS: classSkills[42] ? "On" : "Off" },
+			{ performSingCS: classSkills[43] ? "On" : "Off" },
+			{ profession0CS: classSkills[44] ? "On" : "Off" },
+			{ profession1CS: classSkills[45] ? "On" : "Off" },
+			{ pricraftCS: classSkills[46] ? "On" : "Off" },
+			{ rideCS: classSkills[47] ? "On" : "Off" },
+			{ searchCS: classSkills[48] ? "On" : "Off" },
+			{ senseMotiveCS: classSkills[49] ? "On" : "Off" },
+			{ sleightOfHandCS: classSkills[50] ? "On" : "Off" },
+			{ spellcraftCS: classSkills[51] ? "On" : "Off" },
+			{ spotCS: classSkills[52] ? "On" : "Off" },
+			{ survivalCS: classSkills[53] ? "On" : "Off" },
+			{ swimCS: classSkills[54] ? "On" : "Off" },
+			{ tumbleCS: classSkills[55] ? "On" : "Off" },
+			{ useMagicDevCS: classSkills[56] ? "On" : "Off" },
+			{ usePsionicDevCS: classSkills[57] ? "On" : "Off" },
+			{ useRope: classSkills[58] ? "On" : "Off" }
+		];
+
+		// Spellcaster Options
+		if (this._characterClass instanceof Spellcaster) {
+			// need to distinguish between classes with 0th level spells and otherwise
+			// Can I use a variable for the name of the object?
+			// To iterate thru the spells per day items
+			if (this._characterClass instanceof Paladin || this._characterClass instanceof Ranger) {
+				// levels 1 to 4
+				if (this.spellsPerDay(0) !== undefined) {
+					bodyData.push({ spellsPerDay1: this.spellsPerDay(0) });
+				}
+				if (this.spellsPerDay(1) !== undefined) {
+					bodyData.push({ spellsPerDay2: this.spellsPerDay(1) });
+				}
+				if (this.spellsPerDay(2) !== undefined) {
+					bodyData.push({ spellsPerDay3: this.spellsPerDay(2) });
+				}
+				if (this.spellsPerDay(3) !== undefined) {
+					bodyData.push({ spellsPerDay4: this.spellsPerDay(3) });
+				}
+			}	else if (this._characterClass instanceof Bard) {
+				// levels 1 to 6
+				// Spells Per Day and Spells Known
+				if (this.spellsPerDay(0) !== undefined) {
+					bodyData.push({ spellsPerDay1: this.spellsPerDay(0) });
+					bodyData.push({ spellsKnown1: this.spellsKnown(0) });
+				}
+				if (this.spellsPerDay(1) !== undefined) {
+					bodyData.push({ spellsPerDay2: this.spellsPerDay(1) });
+					bodyData.push({ spellsKnown2: this.spellsKnown(1) });
+				}
+				if (this.spellsPerDay(2) !== undefined) {
+					bodyData.push({ spellsPerDay3: this.spellsPerDay(2) });
+					bodyData.push({ spellsKnown3: this.spellsKnown(2) });
+				}
+				if (this.spellsPerDay(3) !== undefined) {
+					bodyData.push({ spellsPerDay4: this.spellsPerDay(3) });
+					bodyData.push({ spellsKnown4: this.spellsKnown(3) });
+				}
+				if (this.spellsPerDay(4) !== undefined) {
+					bodyData.push({ spellsPerDay5: this.spellsPerDay(4) });
+					bodyData.push({ spellsKnown5: this.spellsKnown(4) });
+				}
+				if (this.spellsPerDay(5) !== undefined) {
+					bodyData.push({ spellsPerDay6: this.spellsPerDay(5) });
+					bodyData.push({ spellsKnown6: this.spellsKnown(5) });
+				}
+			}	else if (this._characterClass instanceof Sorcerer) {
+				// levels 0 to 9 spells per day and spells known
+				if (this.spellsPerDay(0)) {
+					bodyData.push({ spellsPerDay0: this.spellsPerDay(0) });
+					bodyData.push({ spellsKnown0: this.spellsKnown(0) });
+				}
+				if (this.spellsPerDay(1)) {
+					bodyData.push({ spellsPerDay1: this.spellsPerDay(1) });
+					bodyData.push({ spellsKnown1: this.spellsKnown(1) });
+				}
+				if (this.spellsPerDay(2)) {
+					bodyData.push({ spellsPerDay2: this.spellsPerDay(2) });
+					bodyData.push({ spellsKnown2: this.spellsKnown(2) });
+				}
+				if (this.spellsPerDay(3)) {
+					bodyData.push({ spellsPerDay3: this.spellsPerDay(3) });
+					bodyData.push({ spellsKnown3: this.spellsKnown(3) });
+				}
+				if (this.spellsPerDay(4)) {
+					bodyData.push({ spellsPerDay4: this.spellsPerDay(4) });
+					bodyData.push({ spellsKnown4: this.spellsKnown(4) });
+				}
+				if (this.spellsPerDay(5)) {
+					bodyData.push({ spellsPerDay5: this.spellsPerDay(5) });
+					bodyData.push({ spellsKnown5: this.spellsKnown(5) });
+				}
+				if (this.spellsPerDay(6)) {
+					bodyData.push({ spellsPerDay6: this.spellsPerDay(6) });
+					bodyData.push({ spellsKnown6: this.spellsKnown(6) });
+				}
+				if (this.spellsPerDay(7)) {
+					bodyData.push({ spellsPerDay7: this.spellsPerDay(7) });
+					bodyData.push({ spellsKnown7: this.spellsKnown(7) });
+				}
+				if (this.spellsPerDay(8)) {
+					bodyData.push({ spellsPerDay8: this.spellsPerDay(8) });
+					bodyData.push({ spellsKnown8: this.spellsKnown(8) });
+				}
+				if (this.spellsPerDay(9)) {
+					bodyData.push({ spellsPerDay9: this.spellsPerDay(9) });
+					bodyData.push({ spellsKnown9: this.spellsKnown(9) });
+				}
+			}	else {
+				// Wizard, Druid, or Cleric
+				// levels 0 to 9
+				if (this.spellsPerDay(0)) {
+					bodyData.push({ spellsPerDay0: this.spellsPerDay(0) });
+				}
+				if (this.spellsPerDay(1)) {
+					bodyData.push({ spellsPerDay1: this.spellsPerDay(1) });
+				}
+				if (this.spellsPerDay(2)) {
+					bodyData.push({ spellsPerDay2: this.spellsPerDay(2) });
+				}
+				if (this.spellsPerDay(3)) {
+					bodyData.push({ spellsPerDay3: this.spellsPerDay(3) });
+				}
+				if (this.spellsPerDay(4)) {
+					bodyData.push({ spellsPerDay4: this.spellsPerDay(4) });
+				}
+				if (this.spellsPerDay(5)) {
+					bodyData.push({ spellsPerDay5: this.spellsPerDay(5) });
+				}
+				if (this.spellsPerDay(6)) {
+					bodyData.push({ spellsPerDay6: this.spellsPerDay(6) });
+				}
+				if (this.spellsPerDay(7)) {
+					bodyData.push({ spellsPerDay7: this.spellsPerDay(7) });
+				}
+				if (this.spellsPerDay(8)) {
+					bodyData.push({ spellsPerDay8: this.spellsPerDay(8) });
+				}
+				if (this.spellsPerDay(9)) {
+					bodyData.push({ spellsPerDay9: this.spellsPerDay(9) });
+				}
+			}
+		}
 
 		return bodyData;
 	}
