78f2ab17185084627ca3f1fc4c332fbc434e6563
salvis2 <sebialvis@gmail.com>
2018-10-19 11:27:24 -0700

    Created CharacterGenerator.jsx
    Just a dummy component right now, but I will fill it with code later.

---
+++ public/javascripts/CharacterGenerator.jsx
@@ -0,0 +1,13 @@
+// CharacterGenerator.jsx
+import React from "react";
+
+export class CharacterGenerator extends React.Component {
+	render() {
+		let characterGenerator = (
+			<div>
+				<p>Character Generator</p>
+			</div>
+		);
+		return characterGenerator;
+	}
+}
